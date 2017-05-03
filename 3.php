be29b22a1467d740edeb30931c6ed6298cd6dd6b
xwzwmt <18800206482@163.com>
2017-05-03 22:37:28 +0800

    mua

---
+++ 3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	/****
+	布尔教育 高端PHP培训
+	培  训: http://www.itbool.com
+	论  坛: http://www.zixue.it
+	****/
+	
+	echo '这是第 “'._LINE_.'"行';
+	
+	?>
+</body>
+</html>
