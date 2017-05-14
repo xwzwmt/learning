5f886204309bcac1d635507560ba6e3f66f7c208
xwzwmt <18800206482@163.com>
2017-05-14 21:51:49 +0800

    如果名字是中文会咋样“

---
+++ 1.py
@@ -0,0 +1,12 @@
+# -*- coding: cp936 -*-
+print ('我爱我的小师姐')
+temp=input ("猜猜是哪个数字:")        #python中的变量不分类型
+guess=int(temp)
+if guess == 8 :
+    print("哇！师姐超帅的哦")
+    print("可是她有女朋友了")
+else:
+    print("那又怎么样，我会让她喜欢我的")
+print('fishc')
+print('FISHC')
+            
